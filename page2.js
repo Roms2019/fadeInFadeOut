f097de6bc8e11c9ef98800baa0c0a1c74b7b130c
Roms <54295076+Roms2019@users.noreply.github.com>
2019-10-15 17:23:42 -0300

    fadeInFadeOut
    2. No arquivo page2.html, no clique do botão "Fade In" faça com que o alert de sucesso apareça em 5 segundos, no clique do botão "Fade Out" faça com que o alert de erro desapareça em 3 segundos;

---
+++ page2.js
@@ -0,0 +1,18 @@
+//
+//$(function(){
+//   $("#btnSucessFadeIn").click(function(){
+//       $('.alert-sucess').fadeIn(5000);
+//    })
+//    $('#btnDangerFadeOut').click(function(){
+//       
+//    })
+//  })
+
+$(function(){
+    $("#btnSuccessFadeIn").click(function(){
+        $(".alert-success").fadeIn(5000);
+    });
+    $("#btnDangerFadeOut").click(function(){
+        $(".alert-danger").fadeOut(3000);
+    });
+});
